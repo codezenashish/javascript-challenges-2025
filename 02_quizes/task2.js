ab357c01c963284a5a9fcac11c528a959a26be04
codezenashish <ixtashish@gmail.com>
2025-07-13 09:43:04 +0530

    new quiz

--- 02_quizes/task2.js
+++ 02_quizes/task2.js
@@ -1,13 +1,30 @@
-function Q1(){
-    const newStr = 'Hello World'
-    const secondStr = 'javascript'
+// function Q1() {
+//   const newStr = "Hello World";
+//   const secondStr = "javascript";
 
-    let reversingStr = newStr.split('').reverse().join('')
-    let secondReversingStr = secondStr.split('').reverse().join('')
-    console.log(reversingStr)
-    console.log(secondReversingStr)
-    console.log(reversingStr + ' ' + secondReversingStr)
-}
+//   let reversingStr = newStr.split("").reverse().join("");
+//   let secondReversingStr = secondStr.split("").reverse().join("");
+//   console.log(reversingStr);
+//   console.log(secondReversingStr);
+// }
+// Q1();
 
-Q1()
+// const word = "hello world";
+// const vowels = "aeiou";
+// let count = 0;
+// let strCount = "";
+// let x = [];
+
+// for (let i = 0; i < word.length; i++) {
+//   if (vowels.includes(word[i])) {
+//     count++;
+//     strCount += word[i];
+//     x.push(i);
+//   }
+// }
+// console.log(x);
+// console.log(count, strCount);
+
+
+
 
