86822e4b6353fa02b2c3e285ee2739c4f8c2c3da
codezenashish <ixtashish@gmail.com>
2025-08-30 09:09:03 +0530

    Adds string truncation and word reversal functions
    Introduces utility functions for truncating strings to a specified length with ellipsis and for reversing the order of words in a string. Enhances string manipulation capabilities for future problems.

--- set-3/set-3.js
+++ set-3/set-3.js
@@ -2,10 +2,6 @@
 // 🧠 Problem 1: Reverse a String
 // ==============================
 
-
-
-// ========================
-
 function countVowelsInString() {
   let inputString = "An elephant is outside";
   let vowelCharacters = "aeiou";
@@ -27,3 +23,22 @@ function countVowelsInString() {
   console.log(`Indices of vowels = ${vowelIndices}`);
 }
 countVowelsInString();
+
+//  3
+
+function truncate(str, maxLength) {
+  if (str.length > maxLength) {
+    return str.slice(0, maxLength) + "...";
+  }
+  return str;
+}
+
+console.log(truncate("hello ji keshe ho ", 8));
+
+// question number 5
+
+function reverseWord(str) {
+  return str.split(" ").reverse().join();
+}
+
+console.log(reverseWord("hello ashish"));
